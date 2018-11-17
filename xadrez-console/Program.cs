5e1a7c53642a3efc25fdc958879a7ceda1762885
Gerson Barbosa <gersontiger@gmail.com>
2018-11-17 14:38:34 -0300

    Acrescentando as demais pecas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -37,7 +37,8 @@ namespace xadrez_console {
                     }
                 }
 
-                Tela.imprimirTabuleiro(partida.tab);
+                Console.Clear();
+                Tela.imprimirPartida(partida);
             }
             catch (TabuleiroException e) {
                 Console.WriteLine(e.Message);
